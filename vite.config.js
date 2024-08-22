e91454b74c875af710bcca94af488291189c12fc
San-249 <96310661+San-249@users.noreply.github.com>
2024-08-22 18:30:42 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/Conference-event-planner",
+  base: "/Event-planner-website",
   plugins: [react()],
 })
